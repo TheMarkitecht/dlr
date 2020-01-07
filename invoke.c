ee96e57ffe7a9a6747cbd65bc775db0bc5ee8ee0
TheMarkitecht <Mark@TheMarkitecht.com>
2020-01-06 20:50:50 -0600

    Add init().

--- invoke.c
+++ invoke.c
@@ -4,7 +4,26 @@
 #include <dstring.h>
 #include <ffi.h>
 
-void callToNative() {
+// this function's name is based on the library's actual filename.  Jim requires that. 
+extern int Jim_invokeJimInit(Jim_Interp* itp);
+
+int callToNative(Jim_Interp *itp, int objc, Jim_Obj *const objv[]) {
 // ffi_status  ffi_prep_cif(ffi_cif *cif, ffi_abi abi, unsigned int nargs,ffi_type *rtype, ffi_type **atypes);
 // void  ffi_call(ffi_cif *cif, void (*fn)(void), void *rvalue, void **avalue);
+    return JIM_OK;
 }
+
+int Jim_invokeJimInit(Jim_Interp* itp) {
+    //ivkClientT* client = client_alloc(itp);
+
+//todo: Jim_PackageRequire a specific Jim version.
+
+    if (Jim_PackageProvide(itp, "invoke", "0.1", 0) != JIM_OK) {
+        return JIM_ERR;
+    }
+    
+    Jim_CreateCommand(itp,"invoke::callToNative", callToNative, NULL /* client */, NULL);
+    
+    return JIM_OK;
+}
+
