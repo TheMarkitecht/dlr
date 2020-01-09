0ad7bfdaf2a1c628c62f70c85d2d771fe6778f61
TheMarkitecht <Mark@TheMarkitecht.com>
2020-01-09 10:51:58 -0600

    Added prepMetaBlob command, for much more speed.

--- test.tcl
+++ test.tcl
@@ -10,23 +10,26 @@ set ptrFmt 0x%0$($sizeOfPtrBits / 4)X
 pack nullPtr 0 -intle $sizeOfPtrBits
 
 # strtol test
-set invoke::parms {strP endPP radix}
-set invoke::atype0 14 ;# FFI_TYPE_POINTER
-set invoke::atype1 14 ;# FFI_TYPE_POINTER
-set invoke::atype2 10 ;# FFI_TYPE_SINT32
-set invoke::rtype  12 ;# FFI_TYPE_SINT64
-# pack varName value -intle|-intbe|-floatle|-floatbe|-str bitwidth ?bitoffset?
-set myText 556
-set strPUnpack [invoke::addressOfContent myText]
-pack strP $strPUnpack -intle $sizeOfPtrBits
-puts strP=[format $ptrFmt $strPUnpack]
-set endP $nullPtr
-pack endPP [invoke::addressOfContent endP] -intle $sizeOfPtrBits
-pack radix 10 -intle $(8 * [invoke::sizeOfInt])
-invoke::callToNative [invoke::getFnAddr]
-set resultUnpack [unpack $invoke::result -intle 0 $(8 * [invoke::sizeOfInt])]
-puts $myText=$resultUnpack
-# unpack binvalue -intbe|-intle|-uintbe|-uintle|-floatbe|-floatle|-str bitpos bitwidth
-set endPUnpack [unpack $endP -intle 0 $sizeOfPtrBits]
-puts endP=[format $ptrFmt $endPUnpack]
-puts len=$($endPUnpack - $strPUnpack)
+invoke::prepMetaBlob  meta  [invoke::getFnAddr]  result  12  {strP endPP radix}  {14 14 10}
+loop attempt 0 5 {
+    set myText $(550 + $attempt * 3)
+    set strPUnpack [invoke::addressOfContent myText]
+    puts strP=[format $ptrFmt $strPUnpack]
+    # pack varName value -intle|-intbe|-floatle|-floatbe|-str bitwidth ?bitoffset?
+    pack strP $strPUnpack -intle $sizeOfPtrBits
+    
+    set endP $nullPtr
+    pack endPP [invoke::addressOfContent endP] -intle $sizeOfPtrBits
+    
+    pack radix 10 -intle $(8 * [invoke::sizeOfInt])
+    
+    invoke::callToNative  meta
+    
+    # unpack binvalue -intbe|-intle|-uintbe|-uintle|-floatbe|-floatle|-str bitpos bitwidth
+    set resultUnpack [unpack $result -intle 0 $(8 * [invoke::sizeOfInt])]
+    puts $myText=$resultUnpack
+    
+    set endPUnpack [unpack $endP -intle 0 $sizeOfPtrBits]
+    puts endP=[format $ptrFmt $endPUnpack]
+    puts len=$($endPUnpack - $strPUnpack)
+}
