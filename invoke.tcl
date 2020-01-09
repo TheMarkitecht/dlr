6dc16b225196aeebdb78530f811e874ad498f5d7
TheMarkitecht <Mark@TheMarkitecht.com>
2020-01-09 14:41:42 -0600

    Added loadLibNative().
    Implemented fnAddr().
    Started developing invoke.tcl package.
    Renamed invoke.so to invokeNative.so to differentiate from invoke.tcl during package require.
    Renamed addressOf() to addrOf().

--- invoke.tcl
+++ invoke.tcl
@@ -1,3 +1,14 @@
 package provide invoke 0.1
 
-load invoke.so
+package require invokeNative
+
+# system data structures
+set ::invoke::sizeOfPtrBits     $(8 * [::invoke::sizeOfPtr])
+set ::invoke::ptrFmt            0x%0$($::invoke::sizeOfPtrBits / 4)X
+pack nullPtr 0 -intle $::invoke::sizeOfPtrBits
+set ::invoke::libs              [dict create]
+
+proc ::invoke::loadLib {libAlias fileNamePath} {
+    puts $::invoke::libs
+    return [loadLibNative $fileNamePath]
+}
--- test.tcl
+++ test.tcl
@@ -2,34 +2,33 @@ puts $::auto_path
 
 package require invoke
 
-# system data structures
-puts sizeOfInt=[invoke::sizeOfInt]
-puts sizeOfPtr=[invoke::sizeOfPtr]
-set sizeOfPtrBits $(8 * [invoke::sizeOfPtr])
-set ptrFmt 0x%0$($sizeOfPtrBits / 4)X
-pack nullPtr 0 -intle $sizeOfPtrBits
+puts sizeOfInt=[::invoke::sizeOfInt]
+puts sizeOfPtr=[::invoke::sizeOfPtr]
+
+set handle [::invoke::loadLib  testLib  ./invokeTestLib.so]
 
 # strtol test
-invoke::prepMetaBlob  meta  [invoke::getFnAddr]  result  12  {strP endPP radix}  {14 14 10}
+::invoke::prepMetaBlob  meta  [::invoke::fnAddr  test_strtol  $handle]  \
+    result  12  {strP endPP radix}  {14 14 10}
 loop attempt 0 5 {
     set myText $(550 + $attempt * 3)
-    set strPUnpack [invoke::addressOf myText]
-    puts strP=[format $ptrFmt $strPUnpack]
+    set strPUnpack [::invoke::addrOf myText]
+    puts strP=[format $::invoke::ptrFmt $strPUnpack]
     # pack varName value -intle|-intbe|-floatle|-floatbe|-str bitwidth ?bitoffset?
-    pack strP $strPUnpack -intle $sizeOfPtrBits
+    pack strP $strPUnpack -intle $::invoke::sizeOfPtrBits
     
     set endP $nullPtr
-    pack endPP [invoke::addressOf endP] -intle $sizeOfPtrBits
+    pack endPP [::invoke::addrOf endP] -intle $::invoke::sizeOfPtrBits
     
-    pack radix 10 -intle $(8 * [invoke::sizeOfInt])
+    pack radix 10 -intle $(8 * [::invoke::sizeOfInt])
     
-    invoke::callToNative  meta
+    ::invoke::callToNative  meta
     
     # unpack binvalue -intbe|-intle|-uintbe|-uintle|-floatbe|-floatle|-str bitpos bitwidth
-    set resultUnpack [unpack $result -intle 0 $(8 * [invoke::sizeOfInt])]
+    set resultUnpack [unpack $result -intle 0 $(8 * [::invoke::sizeOfInt])]
     puts $myText=$resultUnpack
     
-    set endPUnpack [unpack $endP -intle 0 $sizeOfPtrBits]
-    puts endP=[format $ptrFmt $endPUnpack]
+    set endPUnpack [unpack $endP -intle 0 $::invoke::sizeOfPtrBits]
+    puts endP=[format $::invoke::ptrFmt $endPUnpack]
     puts len=$($endPUnpack - $strPUnpack)
 }
