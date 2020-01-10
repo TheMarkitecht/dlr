c028c816582c2b93ab69537dedde563f1f7630d3
TheMarkitecht <Mark@TheMarkitecht.com>
2020-01-10 16:23:39 -0600

    Added speed benchmark script.

--- .gdbinit
+++ .gdbinit
@@ -4,33 +4,30 @@ db source -output /home/x/debug-source
 db -layout breaks threads stack source
 db source -style context 20
 db assembly -style context 5
-db stack -style limit 5
+db stack -style limit 8
 
 file ./jimsh
-set args test.tcl
+set args bench.tcl
 set cwd .
 set solib-search-path .:..
 
 b jim-load.c:33
 r
 
-b prepMetaBlob
+#b Jim_GenHashFunction
+#commands 
+#    c
+#end
+
+#b prepMetaBlob
 b callToNative
 #b test_strtol
 #b createBufferVar
 #p *meta->cif.arg_types 
 # during break at 191:  watch -l meta->cif.arg_types[0]
-#   b Jim_GetString if objPtr==0x5555555d9000 && lenPtr==0x7fffffffdd44
-# metablob at 0x5555555dbab0 .. 0x5555555dbae8
-#   its arg_types at 0x7fffffffdd40
-#       overlaps lenPtr in Jim_GetString on next script line.
-#       that's in Jim stack space!!  HOW DID ffi_prep_cif GET POINTED TO THAT??
-#       that's  &atypes in prepMetaBlob.
 #display *((metaBlobT*)0x5555555dbab0)->cif.arg_types
 #b Jim_FreeObj if objPtr == 0x5555555dbb00
-#b dlrNative.c:119
-#b dlrNative.c:191
-#b dlrNative.c:214
+#b dlrNative.c:349
 #p script->linenr
 
 c
---
+++ bench.tcl
@@ -0,0 +1,61 @@
+#  "dlr" - Dynamic Library Redux
+#  Copyright 2020 Mark Hubbard, a.k.a. "TheMarkitecht"
+#  http://www.TheMarkitecht.com
+#   
+#  Project home:  http://github.com/TheMarkitecht/dlr
+#  dlr is an extension for Jim Tcl (http://jim.tcl.tk/)
+#  dlr may be easily pronounced as "dealer".
+#   
+#  This file is part of dlr.
+#   
+#  dlr is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#   
+#  dlr is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#   
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with dlr.  If not, see <https://www.gnu.org/licenses/>.
+
+proc assert {exp} {
+    set truth [uplevel 1 [list expr $exp]]
+    if { ! $truth} {
+        error "ASSERT FAILED: $exp"
+    }
+}
+
+puts paths=$::auto_path
+
+package require dlr
+
+puts bitsOfInt=$::dlr::bitsOfInt
+puts bitsOfPtr=$::dlr::bitsOfPtr
+
+::dlr::loadLib  testLib  ./dlrTestLib.so
+
+# strtol speed test
+set ::dlr::lib::testLib::test_strtol::parmOrder {
+    ::dlr::lib::testLib::test_strtol::parm::strP::native
+    ::dlr::lib::testLib::test_strtol::parm::endPP::native
+    ::dlr::lib::testLib::test_strtol::parm::radix::native
+}
+::dlr::prepMetaBlob  meta  [::dlr::fnAddr  test_strtol  testLib]  \
+    ::dlr::lib::testLib::test_strtol::result  12  \
+    $::dlr::lib::testLib::test_strtol::parmOrder  {14 14 10}
+set myNum $(550)
+# addrOf requires a string, so it will implicitly use the string representation of myNum.
+puts strP=[format $::dlr::ptrFmt [::dlr::addrOf myNum]]
+::dlr::pack::ptr  ::dlr::lib::testLib::test_strtol::parm::strP::native  [::dlr::addrOf myNum]
+set endP $::dlr::null
+::dlr::pack::ptr  ::dlr::lib::testLib::test_strtol::parm::endPP::native  [::dlr::addrOf endP]
+::dlr::pack::int  ::dlr::lib::testLib::test_strtol::parm::radix::native  10
+loop attempt 0 30000000 {   
+    ::dlr::callToNative  meta  
+}
+set resultUnpack [::dlr::unpack::int $::dlr::lib::testLib::test_strtol::result]
+puts $myNum=$resultUnpack
+assert {$resultUnpack == $myNum}
